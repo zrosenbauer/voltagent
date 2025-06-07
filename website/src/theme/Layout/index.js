b7207eed5ddce2a661dc2130eff00ff269a783be
Yuichiro Yamashita <xydybaseball@gmail.com>
2025-06-08 00:10:47 +0900

    docs(website): remove duplicated footer (#212)

--- website/src/theme/Layout/index.js
+++ website/src/theme/Layout/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Layout from "@theme-original/Layout";
-import { useLocation } from "@docusaurus/router";
-import { Footer as CustomFooter } from "../../components/footer";
 
 export default function LayoutWrapper(props) {
   return (
     <>
       <Layout {...props} />
-      <CustomFooter />
     </>
   );
 }
